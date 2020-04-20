6ff3a11a28f08a7104d18d2af6472fcb2b77eb2e
Eddie Gomez <eddieegomez4@gmail.com>
2020-04-20 09:50:54 -0700

    bump

--- backend/autoTrade/supportedExchanges/index.js
+++ backend/autoTrade/supportedExchanges/index.js
@@ -6,7 +6,7 @@ const HDMW = require('@oipwg/hdmw')
 const Wallet = HDMW.Wallet
 const axios = require('axios')
 
-
+let min = 60000
 let TotalQty; //Receviced + FeeFloTx1
 let ReceivedQty; //what is deposited from rentals
 let FeeFloTx1; //cumulative fee from rentals.
@@ -22,6 +22,9 @@ let  OfferPriceBtc, //formula
      ProfitUsd // = ( BtcFromTrades * PriceBtcUsd ) - CostOfRentalUsd
 
 module.exports = async function(profile, mnemonic) {
+
+    let orderReceipt;
+
 
     //! fix this later; pass down access token. Refactor later
     accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJUcmluaXR5IiwiaWQiOiI1ZTkxMDM2OTUxZDVlMTI3ZTFlZmJkM2UiLCJpYXQiOjE1ODcwNzAxMTU0NjAsImV4cCI6MTU4NzE1NjUxNTQ2MH0.XDlyIq2F0QP2nB7m6wU_yXlnwZy3mGcjs_XHxfigpkE'
@@ -66,7 +69,6 @@ module.exports = async function(profile, mnemonic) {
 
             const getBalanceFromAddress = async (address) => {
                 try {
-                    console.log('addy', address)
                     let res = await axios.get(`https://livenet.flocha.in/api/addr/${address}`)
 
                     return res.data    
@@ -114,12 +116,11 @@ module.exports = async function(profile, mnemonic) {
                 }
             }
 
-            let floBittrexAddress = getBittrexAddress();
+            let floBittrexAddress = await getBittrexAddress();
 
-            const sendToBittrex = async (wallet) => {
+            const sendToBittrex = async () => {
                 try {
-                    
-                    wallet
+                    myWallet
                     .sendPayment({to: {[floBittrexAddress]: [TotalQty]}})
                     .then(txid => {
                         console.log("Successfully sent Transaction! " + txid)
@@ -134,35 +135,124 @@ module.exports = async function(profile, mnemonic) {
             }
 
             // Sent to Bittrex. Get network Fee for moving tokens
-            let bittrexTX = sendToBittrex();
+            let bittrexTX = await sendToBittrex();
+            bittrexTX='101d071de079de7bb99c238418349bc90f4a04a553137d11a4dc5ad0e45d887e'
 
             if(bittrexTX){
                 try {
+                    console.log('tx', bittrexTX)
                     let res = await axios.get(`https://livenet.flocha.in/api/tx/${bittrexTX}`)
 
                     FeeFloTx2 = res.data.fees
 
                 } catch (error) {
-                    console.log(error)
+                    console.log(error.response)
                 }
             }
+
+            const createSellOrder = async (market, quantity, rate) => {
+                let body = {
+                    market,
+                    quantity,
+                    rate: rate.toFixed(8)
+                }
+                
+                console.log(body)
+
+                try {
+                    const res = await axios.post(`${API_URL}/bittrex/createSellOrder`, body, config)
+                    console.log(res.data)
+                    return res.data.result.uuid
+                } catch (error) {
+                    console.log('error ---', error)
+                }
+                
+            }
+
+
+            CostOfRentalBTC=0.0003686 //! get this form AutoRent
+            TradeFee=1 //!
+            EstFeeBtcTx1=0.00001551 //! get from somewhere
+            // TotalQty=56.40661617
+            // FeeFloTx2=0.000522 //! here cause of rate limited
+
+            OfferPriceBtc = ( CostOfRentalBTC * ( TradeFee + 1 ) * ( margin + 1 ) + EstFeeBtcTx1 ) / ( TotalQty - FeeFloTx1 - FeeFloTx2 )
+        
+            SellableQty = TotalQty - FeeFloTx2
+
+            console.log(
+                { 
+                    TotalQty,
+                    FeeFloTx1,
+                    FeeFloTx2,
+                    SellableQty,
+                    FeeFloTx2, 
+                    CostOfRentalBTC,
+                    TradeFee,
+                    margin,
+                    EstFeeBtcTx1,
+                    TotalQty,
+                    FeeFloTx1,
+                    FeeFloTx2,
+                    OfferPriceBtc
+                
+                }
+                )
 
             try {
 
                 let confirmed = false;
+                let isUpdate = false;
+
+
 
                 const checkConfirmations = async () => {
-
-
                     try {
                         let res = await axios.get(`${API_URL}/bittrex/deposit-history`, config)
 
                         let confirmedList = res.data.result
+
+                        console.log('lame',confirmedList)
     
                         let hasConfirmed = confirmedList.filter(a => a.TxId == bittrexTX)
     
                         if(hasConfirmed){
-                            confirmed = true
+                            if(isUpdate){
+                                //search open order that matches orderReciptID
+                                // update it;
+                                console.log(
+                                    '---Updated---',
+                                    { 
+                                        TotalQty,
+                                        FeeFloTx1,
+                                        FeeFloTx2,
+                                        SellableQty,
+                                        FeeFloTx2, 
+                                        CostOfRentalBTC,
+                                        TradeFee,
+                                        margin,
+                                        EstFeeBtcTx1,
+                                        TotalQty,
+                                        FeeFloTx1,
+                                        FeeFloTx2,
+                                        OfferPriceBtc
+                                    
+                                    })
+
+                                let SellableQtyUp = TotalQty - FeeFloTx2
+                                let OfferPriceBtcUp = ( CostOfRentalBTC * ( TradeFee + 1 ) * ( margin + 1 ) + EstFeeBtcTx1 ) / ( TotalQty - FeeFloTx1 - FeeFloTx2 )
+
+                                SellableQty = 56.40609417;
+                                OfferPriceBtc = 0.00005531
+
+                                SellableQty += SellableQtyUp;
+                                OfferPriceBtc += OfferPriceBtcUp;
+
+                                orderReceiptID = await updateOrder(orderReceipt,token, SellableQty, OfferPriceBtc)
+                            }
+
+                            confirmed = true;
+                            orderReceiptID = await createSellOrder(token, SellableQty, OfferPriceBtc)
                             return clearInterval(timer);
                         }
                     } catch (error) {
@@ -175,70 +265,15 @@ module.exports = async function(profile, mnemonic) {
 
                 const timer = setInterval(() => {
                     checkConfirmations()
-                }, 30000)
+                }, (1 * min))
 
-                CostOfRentalBTC=0.0003686 //! get this form AutoRent
-                TradeFee=1 //!
-                EstFeeBtcTx1=0.00001551 //! get from somewhere
-                // TotalQty=56.40661617
-                // FeeFloTx2=0.000522 //! here cause of rate limited
 
-                OfferPriceBtc = ( CostOfRentalBTC * ( TradeFee + 1 ) * ( margin + 1 ) + EstFeeBtcTx1 ) / ( TotalQty - FeeFloTx1 - FeeFloTx2 )
-            
-                SellableQty = TotalQty - FeeFloTx2
 
-                console.log(
-                    { 
-                        TotalQty,
-                        FeeFloTx1,
-                        FeeFloTx2,
-                        SellableQty,
-                        FeeFloTx2, 
-                        CostOfRentalBTC,
-                        TradeFee,
-                        margin,
-                        EstFeeBtcTx1,
-                        TotalQty,
-                        FeeFloTx1,
-                        FeeFloTx2
-                    
-                    
-                    }
-                    )
 
-                console.log('margin', margin)
-                console.log('offerPrice' , OfferPriceBtc.toFixed(8))
-    
-                //if more FLO/RVN show up in the wallet addres. Send them to Bittrex, update FeeFloTX1, TotalQTR, SellableQTY //TODO:
-                
+             //if more FLO/RVN show up in the wallet addres. Send them to Bittrex, update FeeFloTX1, TotalQTR, SellableQTY //TODO:
 
-                const createSellOrder = async (market, quantity, rate) => {
-                    let body = {
-                        market,
-                        quantity,
-                        rate: rate.toFixed(8)
-                    }
-                    
-                    try {
-                        const res = await axios.post(`${API_URL}/bittrex/createSellOrder`, body, config)
-
-                        console.log(res.data)
-
-                    } catch (error) {
-                        console.log('error ---', error)
-                    }
-                    
-                }
-            
-            let orderReceipt;
-
-            
-
-            if(confirmed && SellableQty){
-                orderReceipt = createSellOrder(token, SellableQty, OfferPriceBtc)
-            }
-
-                console.log('orderReceipt', orderReceipt)
+                //for testing should grab it from the first order created?
+                console.log('orderReceipt', orderReceiptID)
 
                     // BtcFromTrades = cumulative total of Bitcoin earned from trades;
                     // PriceBtcUsd = Coinbase's API - current exchange price for BTC in USD;
@@ -249,7 +284,7 @@ module.exports = async function(profile, mnemonic) {
                             timeout: 1000,
                         });
 
-                        coinbase
+                    return coinbase
                             .get('/exchange-rates?currency=BTC')
                             .then(res => {
                                 console.log('BTC -> USD', res.data.rates.USD)
@@ -260,7 +295,77 @@ module.exports = async function(profile, mnemonic) {
                             });
                     };
 
-                PriceBtcUsd = getCoinbaseBTCUSD()
+                PriceBtcUsd = await getCoinbaseBTCUSD();
+                console.log({PriceBtcUsd})
+
+                let hasUpdated = false;
+
+                const updateOrder = async (orderUuid, market, quantity, rate) => {
+                    let body = {
+                        orderUuid,
+                        market,
+                        quantity,
+                        rate: rate.toFixed(8)
+                    }
+                    
+                    console.log(body)
+
+                    try {
+                        const res = await axios.post(`${API_URL}/bittrex/updateOrder`, body, config)
+                        console.log(res.data)
+                        return res.data.result.uuid
+                    } catch (error) {
+                        console.log('updateOrder ---', error)
+                    }
+                }
+                
+                orderReceiptID = '88cfd377-fb78-40b5-9a4e-125341fe7453'
+                isUpdate=true
+                
+
+                const shouldIUpdated = async () => {
+                    try {
+                        console.log('runing updating')
+                        const res = await getBalanceFromAddress(address);
+
+                        updatedBalance = res.balance
+                        transactions = res.transactions
+
+                        // will need to create a variable for the least amount, I can push up to bittrex wallet.
+                        if(updatedBalance < 50){
+                            return console.log('Not enought to update order', updatedBalance)
+                        }
+
+                        if(updatedBalance > 50){
+                            console.log(updatedBalance)
+                            balance = updatedBalance
+                            isUpdate = true;
+                            //push to walle
+
+                        }
+                        console.log(updatedBalance)
+                        
+                        // let confirmedList = res.data.result
+    
+                        // let hasConfirmed = confirmedList.filter(a => a.TxId == bittrexTX)
+    
+                        if(hasUpdated){
+                            return clearInterval(update);
+                        }
+                    } catch (error) {
+                        console.log(error)
+                    }
+
+                    
+                }
+
+                // const update = setInterval(() => {
+                //     shouldIUpdated()
+                // },(updateUnsold * (60 * min)))
+
+                const update = setInterval(() => {
+                    shouldIUpdated()
+                },(updateUnsold * (min)))
 
 
 
