3e7df2160578d92036b87ccfeb4eb6f83b2918eb
Eddie Gomez <eddieegomez4@gmail.com>
2020-05-19 15:02:00 -0700

    remove user data when logging out

--- src/reducers/setupReducer.js
+++ src/reducers/setupReducer.js
@@ -1,10 +1,20 @@
-import { ADD_PROVIDER, PROVIDER_DATA } from '../actions/types';
+import { 
+    ADD_PROVIDER, 
+    PROVIDER_DATA,
+    LOGOUT_SUCCESS,
+    LOGIN_FAIL,
+    SIGNUP_FAIL,
+} from '../actions/types';
 
 const initState = [];
 export const setupReducer = (state = initState, action) => {
     switch (action.type) {
         case ADD_PROVIDER:
             return action.payload
+        case LOGOUT_SUCCESS:
+        case LOGIN_FAIL:
+        case SIGNUP_FAIL:
+            return [];
         default:
             return state;
     }
@@ -13,6 +23,11 @@ export const login = (state = initState, action) => {
     switch (action.type) {
         case PROVIDER_DATA:
             return action.payload
+        case LOGOUT_SUCCESS:
+        case LOGIN_FAIL:
+        case SIGNUP_FAIL:
+            return [];
+
         default:
             return state;
     }
